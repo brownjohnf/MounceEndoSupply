0d65bebc28181a114279724488fd543843c1746d
John F. Brown <jack@brownjohnf.com>
2012-08-31 19:38:32 +0000

    initialized app; bundled

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MounceEndoSupply::Application.config.secret_token = '5e35cb674697344191f92f98fb63455f1fd0f0e1f65281e6f8407b3ddcf30f2279dcddc97011effe760850a4376eca7f15371c35ce1a919d7ac4921eb1e7934a'
